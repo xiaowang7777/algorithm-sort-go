c9d051a190b42c50639f3c1019973572df57979f
王俊锋 <wang1137095129@foxmail.com>
2022-04-11 23:07:23 +0800

    希尔排序

--- algorithm/sort/shell.go
+++ algorithm/sort/shell.go
@@ -1,1 +1,17 @@
 package sort
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+func ShellSort[T constraints.Ordered](arr []T, step int) {
+	for s := step; s > 0; s-- {
+		for i := s; i < len(arr); i++ {
+			for j := i; j >= s; j -= s {
+				if less(arr, j, j-s) {
+					exch(arr, j, j-s)
+				}
+			}
+		}
+	}
+}

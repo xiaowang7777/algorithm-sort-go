8bffc3920df746521e9dadc65412c7dd94357130
wangjunfeng <junfeng.wang@pitayagames.com>
2022-04-11 11:09:02 +0800

    更正

--- algorithm/sort/insert.go
+++ algorithm/sort/insert.go
@@ -1,17 +1,1 @@
 package sort
-
-import "golang.org/x/exp/constraints"
-
-func InsertSort[T constraints.Ordered](arr []T) {
-	for i := 0; i < len(arr); i++ {
-		minIdx := i
-		for j := i; j < len(arr); j++ {
-			if less(arr, j, minIdx) {
-				minIdx = j
-			}
-		}
-		if i != minIdx {
-			exch(arr, minIdx, i)
-		}
-	}
-}
--- algorithm/sort/selection.go
+++ algorithm/sort/selection.go
@@ -1,1 +1,17 @@
 package sort
+
+import "golang.org/x/exp/constraints"
+
+func SelectionSort[T constraints.Ordered](arr []T) {
+	for i := 0; i < len(arr); i++ {
+		minIdx := i
+		for j := i; j < len(arr); j++ {
+			if less(arr, j, minIdx) {
+				minIdx = j
+			}
+		}
+		if i != minIdx {
+			exch(arr, minIdx, i)
+		}
+	}
+}

8ca41d282b50b4c629af34906c56d75297fb4836
wangjunfeng <junfeng.wang@pitayagames.com>
2022-04-11 11:18:10 +0800

    真~插入排序

--- algorithm/sort/insert.go
+++ algorithm/sort/insert.go
@@ -1,1 +1,15 @@
 package sort
+
+import "golang.org/x/exp/constraints"
+
+func InsertSort[T constraints.Ordered](arr []T) {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0; j-- {
+			if less(arr, j, j-1) {
+				exch(arr, j, j-1)
+			} else {
+				break
+			}
+		}
+	}
+}
--- algorithm/sort/selection_test.go
+++ algorithm/sort/selection_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestInsertSort(t *testing.T) {
+func TestSelectionSort(t *testing.T) {
 	var arr []int = []int{4, 3, 5, 4, 2, 1}
 	var res []int = []int{1, 2, 3, 4, 4, 5}
 	SelectionSort(arr)
